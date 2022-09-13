07d5efda517f4ef66b8b729d07b09dc42b3fbdbf
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-09-13 15:12:59 -0500

    first name text field pointed to after reset button clicked

--- Bishop_FirstApp/Bishop_FirstApp/ViewController.swift
+++ Bishop_FirstApp/Bishop_FirstApp/ViewController.swift
@@ -22,17 +22,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func onClickOfSubmit(_ sender: UIButton) {
+        //Declare variables for text entered in fields
         var first = firstNameTextField.text!
         var last = lastNameTextField.text!
+        
+        //Concatenate first and last name to display in fullNameLabel
         fullNameLabel.text = "Full Name: \(last), \(first)"
+        
+        //Display first index of first and last to display in initialsLabel
         var initial = initialsLabel.text = "Initials: \(first.prefix(1))\(last.prefix(1))"
     }
     
     @IBAction func onClickOfReset(_ sender: UIButton) {
+        //Clear fields of any text
         firstNameTextField.text = ""
         lastNameTextField.text = ""
         fullNameLabel.text = ""
         initialsLabel.text = ""
+        
+        //Make firstNameTextField first responder after field is cleared
+        firstNameTextField.becomeFirstResponder()
+        
     }
     
 }
