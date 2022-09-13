6396eaf1671850d246342f779985af64018d3c60
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-09-13 14:21:35 -0500

    Added UI design and outlets

---
+++ Bishop_FirstApp/Bishop_FirstApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Bishop_FirstApp
+//
+//  Created by Bishop,Mackenzie N on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    
+    @IBOutlet weak var initialsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onClickOfSubmit(_ sender: Any) {
+    }
+    
+    @IBAction func onClickOfReset(_ sender: Any) {
+    }
+    
+}
+
