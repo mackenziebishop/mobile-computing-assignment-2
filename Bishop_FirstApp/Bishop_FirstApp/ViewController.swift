8cba50aff307b32b9894b9d6955679f6eee31ab0
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-09-13 15:04:36 -0500

    Added clear function to reset button

--- Bishop_FirstApp/Bishop_FirstApp/ViewController.swift
+++ Bishop_FirstApp/Bishop_FirstApp/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onClickOfReset(_ sender: UIButton) {
+        firstNameTextField.text = ""
+        lastNameTextField.text = ""
+        fullNameLabel.text = ""
+        initialsLabel.text = ""
     }
     
 }
